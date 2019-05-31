00ca875715ebc0fc4d107bc6600e5fcf387a2d74
kailash@coeus <atkailash@atkailash.me>
2019-05-30 23:55:29 -0600

    Finished IP Subnet, added tests for it

--- 01/ipsubnet.py
+++ 01/ipsubnet.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import argparse
-import warning
 
 argparser = argparse.ArgumentParser(prog="ipsubnet", description="IP CIDT Subnet calculator/checker")
-argparser.add_argument("-f", "--file", metavar='theFile', nargs=1, type=argparse.FileType('r'), help="Filename to parse. Contents in <ip> <cidr> format per line.")
+argparser.add_argument("-f", "--file", nargs=1, type=str, help="Filename to parse. Contents in <ip> <cidr> format per line.")
 argparser.add_argument("-c", "--cidr", nargs=1, type=str, help="CIDR to check")
 argparser.add_argument("-i", "--ip", nargs=1, type=str, help="IP Address to check")
 
-def readFile(inputfile):
+
+def read_file(input_file):
     """
     Parameters:
     filename (str): Name of the file to read
@@ -16,44 +16,39 @@ def readFile(inputfile):
     list_of_ip_cidr (dict): List of lists which are a pair of ip and cidr
     """
 
-    ipcidr = []        
-    currentLine = 1
+    list_of_ip_cidr = []
+    had_error = False
     try:
-        theFile = open(inputfile, "r")
-        for line in theFile:
-            splitLine = line.rstrip("\n").split()
-                if len(splitLine) > 3:
-                    if splitLine[2][0] == "#":
-                        line.remove(splitLine[2])
-                        ipcidr.append(splitLine)
-                    else:
-                       warnings.warn_explicit("Ignoring 3rd column. Comments start with #",
-                                           SyntaxWarning, inputfile, currentLine)
-                else if len(splitLine) == 2):
-                    ipcidr.append(splitLine))
-                else:
-                    raise ValueError("Less than two columns found. Cannot copmare to nothing.")
+        a_file = open(input_file, "r")
+        for line in a_file:
+            split_line = line.rstrip("\n").split("#") # Remove comment first
+            if len(split_line) <= 2: # IP/CDR is [0] and comment is [1], will also work if it's just IP/CDR
+                split_line = split_line[0].split() # Remove the comment
+                list_of_ip_cidr.append(split_line)
+            else:
+                raise ValueError("Less than two columns found. Cannot compare to nothing.")
     except ValueError as ve:
         print(ve)
-        hadError = True
+        had_error = True
     except FileNotFoundError as fnf:
         print(fnf)
-        hadError = True
+        had_error = True
     except PermissionError as perr:
         print(perr)
-        hadError = True
+        had_error = True
     except Exception as e:
         print(e)
-        hadError = True
+        had_error = True
     finally:
-        theFile.close
-        if hadError:
+        a_file.close()
+        if had_error:
             print("Quitting due to error.")
-            quit(-1)
+            quit(4)
         else:
             return list_of_ip_cidr
 
-def convertToBinary(an_ip):
+
+def convert_to_binary(an_ip):
     """
     Takes IP address and converts it to binary
 
@@ -75,13 +70,15 @@ def convertToBinary(an_ip):
         bin_ip = "".join(bin_octet_list)
         return bin_ip
     except ValueError as ve:
-        print ve
-        quit(-1)
-    except TypeError as te:
-        print(f"{octet} is not an integer in {an_ip}")
-        quit(-1)
+        print(ve)
+        print("Quitting: please fix the error")
+        quit(2)
+    except Exception as e:
+        print(e)
+        print("Quitting: please resolve the error")
+        quit(3)
 
-def checkCidr(ip_cidr_list):
+def check_cidr(ip_cidr_list):
     """
     Calculate the cidr of given list that contains IP in 0 and
     the CIDR in 1. This is being done manually instead of using the
@@ -96,33 +93,49 @@ def checkCidr(ip_cidr_list):
 
     ip_address = ip_cidr_list[0]
     [cidr_address, cidr_size] = ip_cidr_list[1].split("/")
-    cidr_size = int(cidr_size) # Make it an int
+    cidr_size = int(cidr_size)  # Make it an int
 
-    bin_cidr_address = convertToBinary(cidr_address)
-    bin_ip_address = convertToBinary(ip_address)
+    bin_cidr_address = convert_to_binary(cidr_address)
+    bin_ip_address = convert_to_binary(ip_address)
 
-    bin_ip_network = ip_address[0:32-(32-cidr_size)]
-    bin_cidr_network = cidr_address[0:32-(32-cidr_size)]
+    bin_ip_network = bin_ip_address[0:cidr_size]
+    bin_cidr_network = bin_cidr_address[0:cidr_size]
 
     return bin_ip_network == bin_cidr_network
 
+
 if __name__ == "__main__":
     args = argparser.parse_args()
-    theFile = args.file
-    theCidr = args.cidr
-    theIp = args.ip
+    the_file = args.file
+    the_cidr = args.cidr
+    the_ip = args.ip
     try:
-        if theFile and (theCidr or theIp):
-            raise argparse.ArgumentError("Cannot have CIDR/IP and File")
-        else if (theCidr == None and theIp) or (theCidr and theIp == None):
-            raise argparse.ArgumentError("Must have both CIDR and IP")
-        else if theFile and (theCidr == None and theIp == None):
-            readFile(theFile)
+        if the_file is not None: # -f is used
+            if the_cidr is not None or the_ip is not None:  # -c or -i is used
+                raise argparse.ArgumentError(the_file, "Cannot have CIDR/IP and File")
+            elif the_cidr is None or the_ip is None:        # no -c or -i
+                list_ip_cidr_list = read_file(the_file[0])
+                for ip_and_cidr in list_ip_cidr_list:
+                    result = check_cidr(ip_and_cidr)
+                    print(f"IP: {ip_and_cidr[0]} in {ip_and_cidr[1]} = {result}")
+            else:
+                raise argparse.ArgumentError()
+        elif the_ip is not None and the_cidr is None:       # -i not -c
+            raise argparse.ArgumentError(the_cidr, "Must have both CIDR and IP")
+        elif the_cidr is not None and the_ip is None:       # -c not -i
+            raise argparse.ArgumentError(the_ip, "Must have both CIDR and IP")
+        elif the_cidr is not None and the_ip is not None:                                               # -c -i not -f
+            the_ip = the_ip[0]
+            the_cidr = the_cidr[0]
+            result = check_cidr([the_ip, the_cidr])
+            print(f"IP: {the_ip} in {the_cidr} = {result}")
         else:
-            checkCidr([theIp, theCidr])
+            argparser.print_help()
+            raise argparse.ArgumentError(the_file, "No valid arguments provided")
     except argparse.ArgumentError as ae:
-        print(ae)
-        quit(-1)
+        print(f"ArgumentError: {ae}")
+        print("Args" + str(vars(args)))
+        quit(1)
     except Exception as e:
-        print(e)
-    finally
+        print(f"Unknown exception: {e}")
+        quit(-1)
---
+++ 01/test_ipsubnet.py
@@ -0,0 +1,18 @@
+import pytest
+import ipsubnet
+
+class TestClass(object):
+    def test_convert_to_binary(self):
+        assert ipsubnet.convert_to_binary("1.1.1.1") == "00000001000000010000000100000001"
+        assert ipsubnet.convert_to_binary("1.0.0.1") != "00000001000000010000000100000001"
+        assert ipsubnet.convert_to_binary("208.67.222.222") == "11010000010000111101111011011110"
+        assert type(ipsubnet.convert_to_binary("1.1.1.1")) == str
+        with pytest.raises(SystemExit, match="2"):
+            ipsubnet.convert_to_binary("256.1.1.1")
+        with pytest.raises(SystemExit, match="2"):
+            ipsubnet.convert_to_binary("a.1.1.1")
+
+    def test_check_cidr(self):
+        assert ipsubnet.check_cidr(["1.1.1.1", "1.0.0.0/15"]) == True
+        assert ipsubnet.check_cidr(["1.0.0.1", "1.0.0.0/15"]) == True
+        assert ipsubnet.check_cidr(["10.1.0.16", "10.1.0.0/28"]) == False
